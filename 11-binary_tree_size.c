d968e40f35ceee3165b3702468adee5002ed5e23
fitsum-kebede <fitsuminfome@gmail.com>
2023-08-30 07:46:57 -0700

    binary_trees

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_size - Measures the size of a binary tree.
- * @node: A pointer to the root node of the tree to measure the size.
+ * @tree: A pointer to the root node of the tree to measure the size.
  * Return: The size of the binary tree. If the node is NULL, returns 0.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
