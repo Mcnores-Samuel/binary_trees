9d50a8a6fc322fcb771b6e975264a27db33defd5
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-08-30 18:04:01 +0200

    updated height implemention

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,19 +1,29 @@
 #include "binary_trees.h"
 
 /**
+ * calculateDepth - calculates the depth of a binary tree
+ * @tree: pointer to the root node of a binary tree.
+ * Return: number or integer representing binary tree depth.
+ */
+size_t calculateDepth(const binary_tree_t *tree)
+{
+	int left_h, right_h;
+	if (tree == NULL)
+		return (0);
+	left_h = calculateDepth(tree->left);
+	right_h = calculateDepth(tree->right);
+
+	return (((left_h > right_h) ? left_h : right_h) + 1) ;
+}
+
+/**
  * binary_tree_height - Calculates the height of a binary tree.
  * @tree: A pointer to the binary tree.
  * Return: The height of the binary tree.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
 
-	if (!tree)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+	size_t height = calculateDepth(tree);
+	return (height - 1);
 }
