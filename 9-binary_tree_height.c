43d37a8d2c3352294f98ee7c682e15a140f17eaf
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-08-30 17:54:01 +0200

    updated height implemention

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
-	return (0);
+	size_t left_height, right_height;
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	if (!tree)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
