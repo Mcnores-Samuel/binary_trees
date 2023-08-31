fd198ac617150394e5963bc8bf6909f75283f1eb
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-08-31 09:28:39 +0200

    measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,36 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * calculateDepth - calculates the depth of a binary tree
- * @tree: pointer to the root node of a binary tree.
- * Return: number or integer representing binary tree depth.
- */
-size_t calculateDepth(const binary_tree_t *tree)
-{
-	int left_h, right_h;
-
-	if (tree == NULL)
-		return (0);
-
-	left_h = calculateDepth(tree->left);
-	right_h = calculateDepth(tree->right);
-
-	return (((left_h > right_h) ? left_h : right_h) + 1);
-}
-
-/**
  * binary_tree_height - Calculates the height of a binary tree.
  * @tree: A pointer to the binary tree.
  * Return: The height of the binary tree.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = calculateDepth(tree);
+	size_t left_height = 0, right_height = 0;
+	if (tree)
+	{	
 
-	if (height > 0)
-		return (height - 1);
-	else
-		return (0);
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+	return (0);
 }
 
 /**
@@ -40,8 +25,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
